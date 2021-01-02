dfa0ece124eeff905583446a0a515effb8bf94ae
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-01-02 17:05:19 +0100

    displaying scores as an array working, now time for saving and Firebase

--- src/script.js
+++ src/script.js
@@ -61,6 +61,13 @@ function stop(){
     changeStartStopBtn();
     isStopBtnClicked = true;
     saveBtn.classList.remove('button--hidden');
+    if (isNextBtnClicked){
+        printLastTime();
+        display.innerHTML = `0:00`;
+    } else if (!isNextBtnClicked){
+        display.classList.add('score')
+    }
+    scoreListDisplay();
 }
 stopBtn.addEventListener('click', stop)
 
@@ -73,19 +80,25 @@ function reset(){
     if (startBtn.classList.contains('button--hidden')){
         changeStartStopBtn();
     }
-}
+    let scores = document.querySelectorAll('.score');
+    scores.forEach((score) => {
+        score.remove()
+    })
+    }
 
 resetBtn.addEventListener('click', reset)
 
 // Next function
 
 const displayContainer = document.querySelector('.display-container');
+let isNextBtnClicked = false;
 
 function nextStart(){
-    console.log(elapsedTime)
     printLastTime()
-    saveBtn.classList.remove('button--hidden');
+    isNextBtnClicked = true;
 }
+
+let listPosition = 1;
 
 function printLastTime(){
     elapsedTime = Date.now() - startTime; // in ms
@@ -93,57 +106,28 @@ function printLastTime(){
     let elapsedLastTimeSeconds = elapsedTimeSeconds;
     let elapsedLastTimeRemainder = elapsedTimeRemainder;
     const displayLast = document.createElement('h2');
-    displayLast.className = 'text-xl mt-8 font-bold font-sans text-indigo-600 small-margins'
+    displayLast.className = `displayLast${listPosition} score text-xl mt-8 font-bold font-sans text-indigo-600 small-margins`
     if(elapsedLastTimeRemainder<10){
         displayLast.innerHTML = `${elapsedLastTimeSeconds}:0${elapsedLastTimeRemainder}`;
     }else{
         displayLast.innerHTML = `${elapsedLastTimeSeconds}:${elapsedLastTimeRemainder}`;
     }
     displayContainer.appendChild(displayLast)
+    listPosition = listPosition +1;
 }
 
 nextBtn.addEventListener('click', nextStart)
 
+// Score list
 
-// let startTime;
-// let elapsedTime = 0;
-// let stopWatchInterval;
-
-// function start(){
-//     if (isStopBtnClicked){
-//         startTime = Date.now();
-//         isStopBtnClicked = false;
-//     } else {
-//         startTime = Date.now() - elapsedTime;
-//     }
-//     stopWatchInterval = setInterval(printTime, 10);
-//     changeStartStopBtn();
-//     pauseBtn.classList.remove('clicked');
-//     saveBtn.classList.add('button--hidden');
-// }
-// startBtn.addEventListener('click', start)
-
-// function printTime(){
-//     elapsedTime = Date.now() - startTime; // in ms
-//     formatTime();
-//     if(elapsedTimeRemainder<10){
-//         display.innerHTML = `${elapsedTimeSeconds}:0${elapsedTimeRemainder}`
-//     }else{
-//         display.innerHTML = `${elapsedTimeSeconds}:${elapsedTimeRemainder}`
-//     }
-// }
-
-// function pause(){
-//     if (!pauseBtn.classList.contains('clicked')){
-//         clearInterval(stopWatchInterval);
-//         changeStartStopBtn();
-//         pauseBtn.classList.add('clicked')
-//     } else {
-//         start();
-//     }
-    
-// }
-// pauseBtn.addEventListener('click', pause)
+function scoreListDisplay(){
+    let scores = document.querySelectorAll('.score')
+    let scoreArray = Array.from(scores)
+    let scoreList = scoreArray.map((score) => {
+        return scoreInnerHTML = score.innerHTML
+    })
+    console.table(scoreList)    
+}
 
 // Formatters and other
 
